fa53f2ae867cec27ba56ecaf44a2e602b6405b4b
ShLL625 <61015no67@gmail.com>
2021-11-03 16:22:58 +0800

    update

---
+++ helpers/data_mining_helpers_newdataset.py
@@ -0,0 +1,45 @@
+import nltk
+
+"""
+Helper functions for data mining lab session 2018 Fall Semester
+Author: Elvis Saravia
+Email: ellfae@gmail.com
+"""
+
+def format_rows(docs):
+    """ format the text field and strip special characters """
+    sentence = []
+    score = []
+    n = 0
+    for i in range(len(docs)):
+        if docs[i] == '\t':
+            sentence.append("".join(docs[n:i]))
+            n = i+1
+        if docs[i] == '\n':
+            score.append("".join(docs[n:i]))
+            n = i+1
+    D = [sentence,score]
+    return D
+
+def format_labels(target, docs):
+    """ format the labels """
+    return docs.target_names[target]
+
+def check_missing_values(row):
+    """ functions that check and verifies if there are missing values in dataframe """
+    counter = 0
+    for element in row:
+        if element == True:
+            counter+=1
+    return ("The amoung of missing records is: ", counter)
+
+def tokenize_text(text, remove_stopwords=False):
+    """
+    Tokenize text using the nltk library
+    """
+    tokens = []
+    for d in nltk.sent_tokenize(text, language='english'):
+        for word in nltk.word_tokenize(d, language='english'):
+            # filters here
+            tokens.append(word)
+    return tokens
